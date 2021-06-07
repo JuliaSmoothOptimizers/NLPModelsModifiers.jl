fa420eed75262d05bc11d5d861142cada5c28aeb
tmigot <tangi.migot@gmail.com>
2021-06-08 00:44:37 +0200

    preallocate meta vectors

--- src/model-interaction.jl
+++ src/model-interaction.jl
@@ -8,11 +8,20 @@ function FeasibilityFormNLS(
   ncon = meta.ncon
   nnzj = meta.nnzj + nequ
   nnzh = meta.nnzh + nequ
+  x0 = similar(meta.x0, nvar)
+  x0[1 : meta.nvar] .= meta.x0
+  x0[meta.nvar : end] .= zero(T)
+  lvar = similar(meta.x0, nvar)
+  lvar[1 : meta.nvar] .= meta.lvar
+  lvar[meta.nvar + 1 : end] .= T(-Inf)
+  uvar = similar(meta.x0, nvar)
+  uvar[1 : meta.nvar] .= meta.uvar
+  uvar[meta.nvar + 1 : end] .= T(Inf)
   meta = NLPModelMeta{T, S}(
     nvar,
-    x0 = [meta.x0; zeros(T, nequ)],
-    lvar = [meta.lvar; fill(T(-Inf), nequ)],
-    uvar = [meta.uvar; fill(T(Inf), nequ)],
+    x0 = x0,
+    lvar = lvar,
+    uvar = uvar,
     ncon = ncon,
     lcon = meta.lcon,
     ucon = meta.ucon,
@@ -23,7 +32,7 @@ function FeasibilityFormNLS(
     nnzh = nnzh,
     name = name,
   )
-  nls_meta = NLSMeta{T, S}(nequ, nvar, x0 = [meta.x0; zeros(T, nequ)], nnzj = nequ, nnzh = 0)
+  nls_meta = NLSMeta{T, S}(nequ, nvar, x0 = x0, nnzj = nequ, nnzh = 0)
 
   nlp = FeasibilityFormNLS{T, S, FeasibilityResidual{T, S}}(meta, nls_meta, nls, NLSCounters())
   finalizer(nlp -> finalize(nlp.internal), nlp)
