40297902d98a1e9eaac4be08d410fb9f73c4e263
tmigot <tangi.migot@gmail.com>
2022-01-06 23:24:23 +0100

    add tailored error for hess_coord/structure of `FeasibilityResidual`

--- src/NLPModelsModifiers.jl
+++ src/NLPModelsModifiers.jl
@@ -7,6 +7,10 @@ using FastClosures
 # jso
 using LinearOperators, NLPModels
 
+macro notimplemented_use_nls(fun)
+    :(error($fun, " is not implemented for models of type FeasibilityResidual. Try converting the FeasibilityResidual model as a FeasbilityFormNLS."))
+end
+
 include("feasibility-form-nls.jl")
 include("feasibility-residual.jl")
 include("quasi-newton.jl")
