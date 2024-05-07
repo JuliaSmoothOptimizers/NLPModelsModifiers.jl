96a1335dc75549166a226481a507368af360d980
tmigot <tmigot@users.noreply.github.com>
2024-05-07 11:05:47 +0000

    :robot: Format .jl files

--- test/gpu_test.jl
+++ test/gpu_test.jl
@@ -21,13 +21,33 @@ function nls_gpu_tests(p, Model; exclude = [])
   end
 end
 
-@testset "Check GPU multiprecision for quasi-Newton model modifiers $M of NLP" for M in [LBFGSModel, LSR1Model, DiagonalPSBModel, DiagonalAndreiModel, SpectralGradientModel]
+@testset "Check GPU multiprecision for quasi-Newton model modifiers $M of NLP" for M in [
+  LBFGSModel,
+  LSR1Model,
+  DiagonalPSBModel,
+  DiagonalAndreiModel,
+  SpectralGradientModel,
+]
   # for hprod, seehttps://github.com/JuliaSmoothOptimizers/LinearOperators.jl/issues/327
-  map(p -> nlp_gpu_tests(p, M, exclude = [hprod, hess, hess_coord, ghjvprod, jth_hess, jth_hess_coord, jth_hprod]), union(NLPModelsTest.nlp_problems, NLPModelsTest.nls_problems))
+  map(
+    p -> nlp_gpu_tests(
+      p,
+      M,
+      exclude = [hprod, hess, hess_coord, ghjvprod, jth_hess, jth_hess_coord, jth_hprod],
+    ),
+    union(NLPModelsTest.nlp_problems, NLPModelsTest.nls_problems),
+  )
 end
 
 @testset "Check GPU multiprecision for model modifiers $M of NLP" for M in [FeasibilityResidual]
-  map(p -> nlp_gpu_tests(p, M, exclude = [hess, hess_coord, ghjvprod, jth_hess, jth_hess_coord, jth_hprod]), setdiff(NLPModelsTest.nlp_problems, ["BROWNDEN", "HS5"]))
+  map(
+    p -> nlp_gpu_tests(
+      p,
+      M,
+      exclude = [hess, hess_coord, ghjvprod, jth_hess, jth_hess_coord, jth_hprod],
+    ),
+    setdiff(NLPModelsTest.nlp_problems, ["BROWNDEN", "HS5"]),
+  )
 end
 
 @testset "Check GPU multiprecision for model modifiers $M of NLP" for M in [SlackModel]
@@ -35,9 +55,19 @@ end
 end
 
 @testset "Check GPU multiprecision for model modifiers $M of NLP" for M in [FeasibilityResidual]
-  map(p -> nls_gpu_tests(p, M, exclude = [hess, hess_coord, ghjvprod, jth_hess, jth_hess_coord, jth_hprod]), setdiff(NLPModelsTest.nls_problems, ["MGH01", "BNDROSENBROCK"]))
+  map(
+    p -> nls_gpu_tests(
+      p,
+      M,
+      exclude = [hess, hess_coord, ghjvprod, jth_hess, jth_hess_coord, jth_hprod],
+    ),
+    setdiff(NLPModelsTest.nls_problems, ["MGH01", "BNDROSENBROCK"]),
+  )
 end
 
-@testset "Check GPU multiprecision for model modifiers $M of NLP" for M in [SlackNLSModel, FeasibilityFormNLS]
+@testset "Check GPU multiprecision for model modifiers $M of NLP" for M in [
+  SlackNLSModel,
+  FeasibilityFormNLS,
+]
   map(p -> nls_gpu_tests(p, M, exclude = []), NLPModelsTest.nls_problems)
 end
