3bff157330b36371915e0c1a28269020988bc753
Maxence Gollier <mgollier45@gmail.com>
2024-07-24 18:20:01 +0200

    add getproperty function for counters

--- src/quasi-newton.jl
+++ src/quasi-newton.jl
@@ -114,8 +114,15 @@ function Base.show(io::IO, nlp::QuasiNewtonModel)
   show(io, nlp.model.counters)
 end
 
+function Base.getproperty(nlp::QuasiNewtonModel, s::Symbol)
+  if s == :counters
+    return nlp.model.counters
+  else 
+    return getfield(nlp, s)
+  end
+end
+
 @default_counters QuasiNewtonModel model
-@default_counters AbstractDiagonalQNModel model
 
 function NLPModels.reset_data!(nlp::QuasiNewtonModel)
   reset!(nlp.op)
