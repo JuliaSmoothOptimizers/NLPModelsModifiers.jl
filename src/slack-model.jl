6667d024bd8735057b1e99912da978522ef1abec
tmigot <tangi.migot@gmail.com>
2024-09-04 20:16:18 -0400

    Add error in slack model for nonsensical constraints

--- src/slack-model.jl
+++ src/slack-model.jl
@@ -131,6 +131,11 @@ function slack_meta(meta::AbstractNLPModelMeta{T, S}; name = meta.name * "-slack
   x0 = similar(meta.x0, meta.nvar + ns)
   x0[1:(meta.nvar)] .= meta.x0
   x0[(meta.nvar + 1):end] .= zero(T)
+
+  if (length(x0) != length(lvar)) && any((nlp.meta.lcon .== -Inf) .& (nlp.meta.ucon .== Inf))
+    throw(error("NLPModel with constraints of the form -∞ ≤ cᵢ(x) ≤ ∞ are not supported by Slack models."))
+  end
+
   return NLPModelMeta{T, S}(
     meta.nvar + ns,
     x0 = x0,
