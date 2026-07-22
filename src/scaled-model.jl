94bd6dd9fa5d394e8b183f496ed2cc4a8178b760
fpacaud <francoispacaud8@gmail.com>
2026-07-22 14:12:27 +0200

    add support for linear and nonlinear API

--- src/scaled-model.jl
+++ src/scaled-model.jl
@@ -78,9 +78,13 @@ struct ScaledModel{T, S, M} <: NLPModels.AbstractNLPModel{T, S}
   meta::NLPModels.NLPModelMeta{T, S}
   counters::NLPModels.Counters
   scaling_obj::T
-  scaling_cons::S # [size m]
-  scaling_jac::S  # [size nnzj]
-  buffer_cons::S  # [size m]
+  scaling_cons::S     # [size m]
+  scaling_cons_lin::S # [size nlin]
+  scaling_cons_nln::S # [size nnln]
+  scaling_jac::S      # [size nnzj]
+  scaling_jac_lin::S  # [size lin_nnzj]
+  scaling_jac_nln::S  # [size nln_nnzj]
+  buffer_cons::S      # [size m]
 end
 
 function ScaledModel(
@@ -90,19 +94,32 @@ function ScaledModel(
   n, m = NLPModels.get_nvar(nlp), NLPModels.get_ncon(nlp)
   x0 = NLPModels.get_x0(nlp)
   buffer_cons  = S(undef, m)
+
+  # Compute scaling for the problem as a whole.
   scaling_obj, scaling_cons, scaling_jac = scale_model!(scaling, nlp)
+
+  # Get scaling for linear and nonlinear constraints.
+  scaling_cons_lin = scaling_cons[nlp.meta.lin]
+  scaling_cons_nln = scaling_cons[nlp.meta.nln]
+  scaling_jac_lin = zeros(T, nlp.meta.lin_nnzj)
+  Jlin_i, Jlin_j = NLPModels.jac_lin_structure(nlp)
+  k = 0
+  for (i, j) in zip(Jlin_i, Jlin_j)
+    scaling_jac_lin[k += 1] = scaling_cons_lin[i]
+  end
+  scaling_jac_nln = zeros(T, nlp.meta.nln_nnzj)
+  Jnln_i, Jnln_j = NLPModels.jac_nln_structure(nlp)
+  k = 0
+  for (i, j) in zip(Jnln_i, Jnln_j)
+    scaling_jac_lin[k += 1] = scaling_cons_nln[i]
+  end
+
+  # Copy metadata from original problem, with some modifications.
   meta = NLPModels.NLPModelMeta(
-    n;
-    lvar=NLPModels.get_lvar(nlp),
-    uvar=NLPModels.get_uvar(nlp),
-    x0=NLPModels.get_x0(nlp),
+    nlp.meta;
     y0 = NLPModels.get_y0(nlp) .* scaling_cons,
-    nnzj=NLPModels.get_nnzj(nlp),
-    nnzh=NLPModels.get_nnzh(nlp),
-    ncon=m,
-    lcon=NLPModels.get_lcon(nlp) .* scaling_cons,
-    ucon=NLPModels.get_ucon(nlp) .* scaling_cons,
-    minimize=nlp.meta.minimize,
+    lcon = NLPModels.get_lcon(nlp) .* scaling_cons,
+    ucon = NLPModels.get_ucon(nlp) .* scaling_cons,
     name="scaled-" * nlp.meta.name,
   )
 
@@ -112,7 +129,11 @@ function ScaledModel(
     NLPModels.Counters(),
     scaling_obj,
     scaling_cons,
+    scaling_cons_lin,
+    scaling_cons_nln,
     scaling_jac,
+    scaling_jac_lin,
+    scaling_jac_nln,
     buffer_cons,
   )
 end
@@ -120,6 +141,13 @@ end
 function NLPModels.obj(nlp::ScaledModel{T, S}, x::AbstractVector) where {T, S <: AbstractVector{T}}
   @lencheck nlp.meta.nvar x
   return nlp.scaling_obj * NLPModels.obj(nlp.nlp, x)
+end
+
+function NLPModels.grad!(nlp::ScaledModel, x::AbstractVector, g::AbstractVector)
+  @lencheck nlp.meta.nvar x g
+  NLPModels.grad!(nlp.nlp, x, g)
+  g .*= nlp.scaling_obj
+  return g
 end
 
 function NLPModels.cons!(nlp::ScaledModel, x::AbstractVector, c::AbstractVector)
@@ -130,11 +158,20 @@ function NLPModels.cons!(nlp::ScaledModel, x::AbstractVector, c::AbstractVector)
   return c
 end
 
-function NLPModels.grad!(nlp::ScaledModel, x::AbstractVector, g::AbstractVector)
-  @lencheck nlp.meta.nvar x g
-  NLPModels.grad!(nlp.nlp, x, g)
-  g .*= nlp.scaling_obj
-  return g
+function NLPModels.cons_lin!(nlp::ScaledModel, x::AbstractVector, c::AbstractVector)
+  @lencheck nlp.meta.nvar x
+  @lencheck nlp.meta.nlin c
+  NLPModels.cons_lin!(nlp.nlp, x, c)
+  c .*= nlp.scaling_cons_lin
+  return c
+end
+
+function NLPModels.cons_nln!(nlp::ScaledModel, x::AbstractVector, c::AbstractVector)
+  @lencheck nlp.meta.nvar x
+  @lencheck nlp.meta.nnln c
+  NLPModels.cons_nln!(nlp.nlp, x, c)
+  c .*= nlp.scaling_cons_nln
+  return c
 end
 
 function NLPModels.jprod!(nlp::ScaledModel, x::AbstractVector, v::AbstractVector, Jv::AbstractVector)
@@ -142,6 +179,22 @@ function NLPModels.jprod!(nlp::ScaledModel, x::AbstractVector, v::AbstractVector
   @lencheck nlp.meta.ncon Jv
   NLPModels.jprod!(nlp.nlp, x, v, Jv)
   Jv .*= nlp.scaling_cons
+  return Jv
+end
+
+function NLPModels.jprod_lin!(nlp::ScaledModel, x::AbstractVector, v::AbstractVector, Jv::AbstractVector)
+  @lencheck nlp.meta.nvar x v
+  @lencheck nlp.meta.nlin Jv
+  NLPModels.jprod_lin!(nlp.nlp, x, v, Jv)
+  Jv .*= nlp.scaling_cons_lin
+  return Jv
+end
+
+function NLPModels.jprod_nln!(nlp::ScaledModel, x::AbstractVector, v::AbstractVector, Jv::AbstractVector)
+  @lencheck nlp.meta.nvar x v
+  @lencheck nlp.meta.nnln Jv
+  NLPModels.jprod_lin!(nlp.nlp, x, v, Jv)
+  Jv .*= nlp.scaling_cons_nln
   return Jv
 end
 
@@ -154,14 +207,55 @@ function NLPModels.jtprod!(nlp::ScaledModel, x::AbstractVector, v::AbstractVecto
   return Jtv
 end
 
+function NLPModels.jtprod_lin!(nlp::ScaledModel, x::AbstractVector, v::AbstractVector, Jtv::AbstractVector)
+  @lencheck nlp.meta.nvar x Jtv
+  @lencheck nlp.meta.nlin v
+  v_scaled = view(nlp.buffer_cons, 1:nlp.meta.nlin)
+  v_scaled .= v .* nlp.scaling_cons_lin
+  NLPModels.jtprod_lin!(nlp.nlp, x, v_scaled, Jtv)
+  return Jtv
+end
+
+function NLPModels.jtprod_nln!(nlp::ScaledModel, x::AbstractVector, v::AbstractVector, Jtv::AbstractVector)
+  @lencheck nlp.meta.nvar x Jtv
+  @lencheck nlp.meta.nnln v
+  v_scaled = view(nlp.buffer_cons, 1:nlp.meta.nnln)
+  v_scaled .= v .* nlp.scaling_cons_nln
+  NLPModels.jtprod_nln!(nlp.nlp, x, v_scaled, Jtv)
+  return Jtv
+end
+
 function NLPModels.jac_structure!(nlp::ScaledModel, jrows::AbstractVector, jcols::AbstractVector)
+  @lencheck nlp.meta.nnzj jrows jcols
   NLPModels.jac_structure!(nlp.nlp, jrows, jcols)
+  return jrows, jcols
+end
+
+function NLPModels.jac_lin_structure!(nlp::ScaledModel, jrows::AbstractVector, jcols::AbstractVector)
+  NLPModels.jac_lin_structure!(nlp.nlp, jrows, jcols)
+  return jrows, jcols
+end
+
+function NLPModels.jac_nln_structure!(nlp::ScaledModel, jrows::AbstractVector, jcols::AbstractVector)
+  NLPModels.jac_nln_structure!(nlp.nlp, jrows, jcols)
   return jrows, jcols
 end
 
 function NLPModels.jac_coord!(nlp::ScaledModel, x::AbstractVector, jac::AbstractVector)
   NLPModels.jac_coord!(nlp.nlp, x, jac)
   jac .*= nlp.scaling_jac
+  return jac
+end
+
+function NLPModels.jac_lin_coord!(nlp::ScaledModel, x::AbstractVector, jac::AbstractVector)
+  NLPModels.jac_lin_coord!(nlp.nlp, x, jac)
+  jac .*= nlp.scaling_jac_lin
+  return jac
+end
+
+function NLPModels.jac_nln_coord!(nlp::ScaledModel, x::AbstractVector, jac::AbstractVector)
+  NLPModels.jac_nln_coord!(nlp.nlp, x, jac)
+  jac .*= nlp.scaling_jac_nln
   return jac
 end
 
